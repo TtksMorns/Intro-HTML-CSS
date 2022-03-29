f3293125d962fd727ced6cca0149c9f100f0ab0f
TtksMorns <ttviniciusks@gmail.com>
2022-03-28 21:32:01 -0300

    ProjetinhoWeb

---
+++ Exercicios/Ex2.2/index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<table>
+  <tr>
+    <th class="posicao">Posição</th>
+    <th>Cidade</th>
+    <th>Pais</th>
+    <th class="quant">Homicido</th>
+    <th class="quant">Habitantes</th>
+    <th class="quant">Taxa(por cada 100 mil habitantes)</th>
+  </tr>
+  <tr>
+    <td class="posicao">1</td>
+    <td>Los Cabos</td>
+    <td>México</td>
+    <td class="quant">365</td>
+    <td class="quant">328.245</td>
+    <td class="quant">111,33</td>
+  </tr>
+  <tr>
+    <td class="posicao">1</td>
+    <td>Los Cabos</td>
+    <td>México</td>
+    <td class="quant">365</td>
+    <td class="quant">328.245</td>
+    <td class="quant">111,33</td>
+  </tr>
+  <tr>
+    <td class="posicao">1</td>
+    <td>Los Cabos</td>
+    <td>México</td>
+    <td class="quant">365</td>
+    <td class="quant">328.245</td>
+    <td class="quant">111,33</td>
+  </tr>
+  <tr>
+    <td class="posicao">1</td>
+    <td>Los Cabos</td>
+    <td>México</td>
+    <td class="quant">365</td>
+    <td class="quant">328.245</td>
+    <td class="quant">111,33</td>
+  </tr>
+  <tr>
+    <td class="posicao">1</td>
+    <td>Los Cabos</td>
+    <td>México</td>
+    <td class="quant">365</td>
+    <td class="quant">328.245</td>
+    <td class="quant">111,33</td>
+  </tr>
+  <tr>
+    <td class="posicao">1</td>
+    <td>Los Cabos</td>
+    <td>México</td>
+    <td class="quant">365</td>
+    <td class="quant">328.245</td>
+    <td class="quant">111,33</td>
+  </tr>
+  <tr>
+    <td class="posicao">1</td>
+    <td>Los Cabos</td>
+    <td>México</td>
+    <td class="quant">365</td>
+    <td class="quant">328.245</td>
+    <td class="quant">111,33</td>
+  </tr>
+  <tr>
+    <td class="posicao">1</td>
+    <td>Los Cabos</td>
+    <td>México</td>
+    <td class="quant">365</td>
+    <td class="quant">328.245</td>
+    <td class="quant">111,33</td>
+  </tr>
+  <tr>
+    <td class="posicao">1</td>
+    <td>Los Cabos</td>
+    <td>México</td>
+    <td class="quant">365</td>
+    <td class="quant">328.245</td>
+    <td class="quant">111,33</td>
+  </tr>
+</table>
+
+</body>
+</html>
